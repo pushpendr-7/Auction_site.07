3afe8d958f68753d18d73ae4bf1d775a4c6c5d42
Tred <githut709@gmail.com>
2025-10-24 21:10:01 +0530

    Updatebsbhdhsb settings.py

--- auction_site/settings.py
+++ auction_site/settings.py
@@ -20,7 +20,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'auctions', 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Quick-start development settings - unsuitable for production
