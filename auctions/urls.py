54d80afe58737590dd84666489385da7a7933bb0
Tred <githut709@gmail.com>
2025-10-27 13:38:02 +0530

    Update1 urls.py

--- auctions/urls.py
+++ auctions/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('index/', views.index, name='index'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('register/', views.register_view, name='register'),
